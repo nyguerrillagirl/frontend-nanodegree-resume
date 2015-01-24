3c42e778895a6de2be67900ca18249ec4183fdd6
Lorraine Figueroa <nyguerrillagirl@brainycode.com>
2015-01-24 14:32:47 -0500

    Project 2 work

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,6 +1,6 @@
 // resumeBuilder.js
 
-
+// This contains data objects specific to my bio, skills and projects 
 
 
 var skills = ["Java", "C++", "Spring Framework", "Writing", "HTML5", "CSS3", "JavaScript",  "R", "Teaching"];
@@ -9,8 +9,8 @@ var skills = ["Java", "C++", "Spring Framework", "Writing", "HTML5", "CSS3", "Ja
 var bio = {
 	"name" : "Lorraine Figueroa",
 	"role" : "Software Engineer",
-	"welcomeMessage" : "Hi, this is my resume - impressed yet??",
-	"picture" : "images/fry.jpg",
+	"welcomeMessage" : "Java Developer, Learning Web Development!",
+	"biopic" : "images/avatar03.jpg",
 	"contacts" : {
 		"mobile" : "555-555-5555",
 		"email" : "nyguerrillagirl@brainycode.com",
@@ -18,42 +18,49 @@ var bio = {
 		"twitter" : "@nyguerrillagirl",
 		"location" : "Philadelphia"
 	},
-	"skills" : skills
-};
+	"skills" : skills,
+	"display" : function() {
+		// Display the name and title
+		var formattedName = HTMLheaderName.replace("%data%", "Lorraine Figueroa");
+		var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
 
-bio.display = function() {
+		$("#header").prepend(formattedRole);
+		$("#header").prepend(formattedName);
+		// Display the various contact information
+		if (bio.contacts) {
+			// append contacts to the top
+			for (contactIterator in bio.contacts) {
+				var contactInfo = contactIterator + " " + bio.contacts[contactIterator];
+				// topContacts
+				contactInfo = HTMLcontactGeneric.replace("%contact%", contactIterator);
+				contactInfo = contactInfo.replace("%data%", bio.contacts[contactIterator]);
+				$("#footerContacts").append(contactInfo);
+			}
 
-	var formattedName = HTMLheaderName.replace("%data%", "Lorraine Figueroa");
-	var formattedRole = HTMLheaderRole.replace("%data%", "Software Engineer");
+			// append contacts to the footer
+			for (contactIterator in bio.contacts) {
+				var contactInfo = contactIterator + " " + bio.contacts[contactIterator];
+				contactInfo = HTMLcontactGeneric.replace("%contact%", contactIterator);
+				contactInfo = contactInfo.replace("%data%", bio.contacts[contactIterator]);
+				$("").append(contactInfo);
+			}
 
-	$("#header").prepend(formattedRole);
-	$("#header").prepend(formattedName);
-
-	if (bio.contacts) {
-		for (contactIterator in bio.contacts) {
-			var contactInfo = contactIterator + " " + bio.contacts[contactIterator];
-			console.log(contactInfo);
-			// topContacts
-			contactInfo = HTMLcontactGeneric.replace("%contact%", contactIterator);
-			contactInfo = contactInfo.replace("%data%", bio.contacts[contactIterator]);
-			$("#topContacts").append(contactInfo);
 		}
-	}
+		// Display my image and Welcome message
+	    if (bio.biopic) {
+			$("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
+		}
+		$("#header").append(HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage));
 
-	//if (bio.picture) {
-	//	$("#skills").append(HTMLbioPic.replace("%data%", bio.picture));
-	//}
-	//$("#skills").append(HTMLWelcomeMsg.replace("%data%", bio.welcomeMessage));
+		if (bio.skills && (bio.skills.length > 0)) {
+			// append the skills to the page
+			$("#header").append(HTMLskillsStart);
 
-	if (bio.skills && (bio.skills.length > 0)) {
-		// append the skills to the page
-		$("#header").append(HTMLskillsStart);
+			for (var i=0; i < bio.skills.length; i++) {
+				$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
+			}	
 
-
-
-		for (var i=0; i < bio.skills.length; i++) {
-			$("#skills").append(HTMLskills.replace("%data%", bio.skills[i]));
-		}	
+		}
 
 	}
 };
@@ -63,43 +70,90 @@ bio.display = function() {
 var education = {
 	"schools" : [
 		{
-			"name" : "City College",
+			"name" : "City College of New York",
 			"location" : "NYC",
 			"degree" : "BS",
-			"major": "Computer Science",
-			"graduation" : "1987",
+			"majors": "Computer Science",
+			"dates" : "1987",
 			"url" : "http://www.ccny.cuny.edu/"
 		},
 		{
-			"name" : "City College",
+			"name" : "City College of New York",
 			"location" : "NYC",
 			"degree" : "MS",
-			"major": "Computer Science",
-			"graduation" : "1989"
-
+			"majors": "Computer Science",
+			"dates" : "1989",
+			"url" : "http://www.ccny.cuny.edu/"
 		}
 	],
 	"onlineCourses" : [	{
 			"school" : "Coursera",
-			"location" : "N/A",
 			"title": "The Data Scientist’s Toolbox",
-			"dates" : "12/2014",
+			"date" : "12/2014",
+			"url" : "https://www.coursera.org/"
+		},{
+			"school" : "Coursera",
+			"title": "R Programming",
+			"date" : "1/2015",
 			"url" : "https://www.coursera.org/"
 		},
 		{
 			"school" : "Udacity",
 			"title": "HTML and CSS",
-			"dates" : "12/2014",
+			"date" : "01/2015",
 			"url" : "https://www.udacity.com/"
 		},
 		{
 			"school" : "Udacity",
 			"title": "Git and GitHub",
-			"dates" : "12/2014 - 1/2015",
+			"date" : "1/2015",
+			"url" : "https://www.udacity.com/"
+		},
+		{
+			"school" : "Udacity",
+			"title": "JavaScript Basics",
+			"date" : "1/2015",
+			"url" : "https://www.udacity.com/"
+		},
+		{
+			"school" : "Udacity",
+			"title": "Intro to jQuery",
+			"date" : "1/2015",
 			"url" : "https://www.udacity.com/"
 		}
+	],
+	"display" : function() {
+		// display formal education information
+		if (education.schools && education.schools.length > 0) {
+			for (school in education.schools) {
+				// add an education entry div element
+				$("#education").append(HTMLschoolStart);
+				var edLine1 = HTMLschoolName.replace("%data%", education.schools[school].name) + 
+									HTMLschoolDegree.replace("%data%", education.schools[school].degree)
+				$(".education-entry").last().append(edLine1);
+				$(".education-entry").last().append(HTMLschoolDates.replace("%data%", education.schools[school].dates));
+				$(".education-entry").last().append(HTMLschoolLocation.replace("%data%", education.schools[school].location));
+				$(".education-entry").last().append(HTMLschoolMajor.replace("%data%", education.schools[school].majors));
+			}
+		}
 
-	]
+		// display online education information
+		if (education.onlineCourses && education.onlineCourses.length > 0) {
+			$("#education").append(HTMLonlineClasses);
+			for (school in education.onlineCourses) {
+				// add an education entry div element
+				$("#education").append(HTMLschoolStart);
+				var edLine1 = HTMLonlineTitle.replace("%data%", education.onlineCourses[school].title) + 
+								HTMLonlineSchool.replace("%data%", education.onlineCourses[school].school);
+				$(".education-entry").last().append(edLine1);
+				$(".education-entry").last().append(HTMLonlineDates.replace("%data%", education.onlineCourses[school].date));
+				$(".education-entry").last().append(HTMLonlineURL.replace("%data%", education.onlineCourses[school].url));
+			}
+
+		}
+
+
+	}
 };
 
 var work = {
@@ -122,31 +176,32 @@ var work = {
 		"title" : "Computer Studies Lecturer",
 		"location" : "Toms River, NJ",
 		"dates" : "9/2000 - 6/2009",
-		"description" : "Taught courses on web design, C++, Java, and Game Design."
-	 }]
+		"description" : "Taught courses on Assembly Language, C++, Java, and Game Design."
+	 }],
+	 "display" : function () {
+		// Output all the jobs in our work object
+		for (jobIterator in work.jobs) {
+			//alert("processing a job: " + work.jobs[jobIterator].employer);
+			$("#workExperience").append(HTMLworkStart);
+			var jobEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIterator].employer);
+			var jobTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIterator].title);
+			$(".work-entry:last").append(jobEmployer + jobTitle);
+			$(".work-entry:last").append(HTMLworkLocation.replace("%data%",  work.jobs[jobIterator].location));
+			$(".work-entry:last").append(HTMLworkDates.replace("%data%",  work.jobs[jobIterator].dates));
+			$(".work-entry:last").append(HTMLworkDescription.replace("%data%",  work.jobs[jobIterator].description));
+
+		}
+	}
 };
 
-function displayWork() {
-	// Output all the jobs in our work object
-	for (jobIterator in work.jobs) {
-		//alert("processing a job: " + work.jobs[jobIterator].employer);
-		$("#workExperience").append(HTMLworkStart);
-		var jobEmployer = HTMLworkEmployer.replace("%data%", work.jobs[jobIterator].employer);
-		var jobTitle = HTMLworkTitle.replace("%data%", work.jobs[jobIterator].title);
-		$(".work-entry:last").append(jobEmployer + jobTitle);
-		$(".work-entry:last").append(HTMLworkLocation.replace("%data%",  work.jobs[jobIterator].location));
-		$(".work-entry:last").append(HTMLworkDates.replace("%data%",  work.jobs[jobIterator].dates));
-		$(".work-entry:last").append(HTMLworkDescription.replace("%data%",  work.jobs[jobIterator].description));
 
-	}
-}
 
 
 var projects= {
 	"projects" : [ {
 		"title" : "Nanodegree in Front-End Web Development",
 		"dates" : "12/2014 - Present",
-		"description" : "Series of courses learning tools and technology for web development",
+		"description" : "Taking a series of courses to learn tools and technology for web development",
 		"images" : [
 			"images/HTML5_Logo_512.png", "images/css3-logo.png", "images/javascript-logo-png.png", "images/bootstrap.png", "images/git-logo.png", "images/GitHub.jpg"
 		]
@@ -154,50 +209,44 @@ var projects= {
 	{
 		"title" : "John Hopkins University Data Science",
 		"dates" : "12/2014 - Present",
-		"description" : "Series of courses learning tools and technology for Data Science",
+		"description" : "Taking a series of courses learning tools and technology for Data Science",
 		"images" : [
 			"images/bloomberg.small.horizontal.blue-Coursera.png", "images/R-Programming.png"
 		]
+	},
+	{
+		"title" : "Exploring Mini-Languages to Teach kids how to code",
+		"dates" : "06/2014 - Present",
+		"description" : "Hosting Talks on Learning to Make Games using Java, Scratch, Alice, Kodu, Karel The Robot",
+		"images" : ["images/JavaLogo.png", "images/ScratchBlogLogo.jpg", "images/alice3_logo.png", "images/kodu2.jpg", "images/KarelLogo.jpg"]
 	}
-	]
+	],
+	"display" : function() {
+				for (projectIterator in projects.projects) {
+					$("#projects").append(HTMLprojectStart);
+					var projectTitle = HTMLprojectTitle.replace("%data%", projects.projects[projectIterator].title);
+					var projectDates = HTMLprojectDates.replace("%data%", projects.projects[projectIterator].dates);
+					var projectDescription = HTMLprojectDescription.replace("%data%", projects.projects[projectIterator].description);
+					$(".project-entry:last").append(projectTitle);
+					$(".project-entry:last").append(projectDates);
+					$(".project-entry:last").append(projectDescription);	
+					// process each image associated with this project
+					for (imageIterator in projects.projects[projectIterator].images) {
+						var aProjectImage = HTMLprojectImage.replace("%data%", projects.projects[projectIterator].images[imageIterator]);
+						$(".project-entry:last").append(aProjectImage);
+					}
+				}
+			}
 };
 
-projects.display = function() {
-	for (projectIterator in projects.projects) {
-		$("#projects").append(HTMLprojectStart);
-		var projectTitle = HTMLprojectTitle.replace("%data%", projects.projects[projectIterator].title);
-		var projectDates = HTMLprojectDates.replace("%data%", projects.projects[projectIterator].dates);
-		var projectDescription = HTMLprojectDescription.replace("%data%", projects.projects[projectIterator].description);
-		$(".project-entry:last").append(projectTitle);
-		$(".project-entry:last").append(projectDates);
-		$(".project-entry:last").append(projectDescription);	
-		// process each image associated with this project
-		for (imageIterator in projects.projects[projectIterator].images) {
-			var aProjectImage = HTMLprojectImage.replace("%data%", projects.projects[projectIterator].images[imageIterator]);
-			$(".project-entry:last").append(aProjectImage);
-		}
-	}
-};
-
-
-$(document).click(function(loc) {
-	logClicks(loc.pageX, loc.pageY);
-});
-
-function inName(name) {
-	alert("inName invoked with name: " + name);
-	var arrNames = name.split(" ");
-	var firstName = arrNames[0];
-	var lastName = arrNames[1];
-	firstName = firstName.substr(0,1).toUpperCase() + firstName.substr(1).toLowerCase();
-	lastName = lastName.toUpperCase();
-	return firstName + " " + lastName;
+function displayResumeInformation() {
+	// Populate page with RESUME information - HERE
+	bio.display();
+	work.display();
+	projects.display();
+	education.display();
+	$("#mapDiv").append(googleMap);
 }
-// The name game
-//$("#main").append(internationalizeButton);
 
-bio.display();
-displayWork();
-projects.display();
+displayResumeInformation();
 
-$("#mapDiv").append(googleMap);
